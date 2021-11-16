4c28c81d0e1deb19eda5aa66ed202ce3a3105c68
jamilligioielli <jamilligioielli@outlook.com>
2021-11-16 17:49:14 -0300

    updating webpack version

--- Microfrontends/ecomm/products/webpack.config.js
+++ Microfrontends/ecomm/products/webpack.config.js
@@ -1,3 +1,13 @@
+//This will set the webpack js file names in the HTML script tag
+const HTMLWebpackPlugin = require('html-webpack-plugin')
 module.exports = {
     mode: 'development',
+    devServer: {
+        port:8081
+    }, 
+    plugins: [
+        new HTMLWebpackPlugin({
+            template: './public/index.html'
+        })
+    ]
 };

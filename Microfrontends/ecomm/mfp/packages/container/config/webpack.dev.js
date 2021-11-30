adae6aa0090316f38be7389f4a8532ab62523ff9
jamilligioielli <jamilligioielli@outlook.com>
2021-11-29 22:34:24 -0300

    Sections 4 and 5 [Done]

---
+++ Microfrontends/ecomm/mfp/packages/container/config/webpack.dev.js
@@ -0,0 +1,32 @@
+// this function will merge it with common.js
+const { merge } = require('webpack-merge')
+const HTMLWebpackPlugin = require('html-webpack-plugin')
+const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin')
+const commonConfig = require('./webpack.common')
+const packageJson = require('../package.json')
+
+
+const devConfig = {
+	mode: 'development',
+	devServer: {
+		port: 8080,
+		historyApiFallback: {
+			index: 'index.html'
+		}
+	},
+	plugins: [
+		new ModuleFederationPlugin({
+			name: 'container',
+			remotes: {
+				marketing: 'marketing@http://localhost:8081/remoteEntry.js'
+			},
+			shared: packageJson.dependencies,
+		}),
+		new HTMLWebpackPlugin({
+			template: './public/index.html'
+		})
+	]
+}
+
+// devconfig will overrride any options of commonconfig
+module.exports =  merge(commonConfig, devConfig)
